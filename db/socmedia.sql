cfbcaf4bf994db9481a23ada9541de1c786dfdab
InfoSystemsThanjavaur <150105991+InfoSystemsThanjavaur@users.noreply.github.com>
2024-12-21 15:59:47 +0530

    Add files via upload

---
+++ db/socmedia.sql
@@ -0,0 +1,27 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.0.27-community-nt - MySQL Community Edition (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             12.5.0.6677
+-- ------------------------------------
+-- Data exporting was unselected.
+
+-- Data exporting was unselected.
+
+-- Data exporting was unselected.
+
+-- Data exporting was unselected.
+
+-- Data exporting was unselected.
+
+-- Data exporting was unselected.
+
+-- Data exporting was unselected.
+
+-- Data exporting was unselected.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
